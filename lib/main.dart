faf878374ab0a9e2b4b5bc061728964440de8822
RazanHammad97 <razanhammadq@gmail.com>
2023-02-12 23:02:56 +0200

    edit the design

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'القران الكريم',
       theme: ThemeData(
-        primarySwatch: Colors.green,
+        primarySwatch: buildMaterialColor(Color(0xE2195E59)),
       ),
 
       home: const SplashScreen(title: "القران الكريم",)
@@ -79,14 +79,14 @@ class _MyHomePageState extends State<MyHomePage> {
                                 height: 35,
                                 child: Stack(
                                     children: [Image(
-                                      color: Colors.green,
+                                      color: Color(0xE2195E59),
                                       image: AssetImage("surahnum.png"),),
                                       Center(child: Directionality(
                                           textDirection: TextDirection.rtl,
-                                          child: Text("${index+1}",style: TextStyle(color: Colors.green),)),)
+                                          child: Text("${index+1}",style: TextStyle(color: Color(0xE2195E59)),)),)
                                     ]),
                               ),
-                              trailing: Icon(Icons.keyboard_arrow_left),
+                              trailing: Icon(Icons.keyboard_arrow_left,color: Color(0xE2195E59),),
                               onTap: () {
                                 // print(qb.getSurahAyahs(surahNum: index + 1));
                                 print('tapp');
@@ -118,4 +118,24 @@ class _MyHomePageState extends State<MyHomePage> {
           )),
     );
   }
+}
+
+MaterialColor buildMaterialColor(Color color) {
+  List strengths = <double>[.05];
+  Map<int, Color> swatch = {};
+  final int r = color.red, g = color.green, b = color.blue;
+
+  for (int i = 1; i < 10; i++) {
+    strengths.add(0.1 * i);
+  }
+  strengths.forEach((strength) {
+    final double ds = 0.5 - strength;
+    swatch[(strength * 1000).round()] = Color.fromRGBO(
+      r + ((ds < 0 ? r : (255 - r)) * ds).round(),
+      g + ((ds < 0 ? g : (255 - g)) * ds).round(),
+      b + ((ds < 0 ? b : (255 - b)) * ds).round(),
+      1,
+    );
+  });
+  return MaterialColor(color.value, swatch);
 }
--- lib/splashscreen.dart
+++ lib/splashscreen.dart
@@ -3,8 +3,9 @@ import 'package:flutter/material.dart';
 import 'main.dart';
 
 class SplashScreen extends StatefulWidget {
-  const SplashScreen({Key? key,required this.title}) : super(key: key);
-final String title;
+  const SplashScreen({Key? key, required this.title}) : super(key: key);
+  final String title;
+
   @override
   State<SplashScreen> createState() => _SplashScreenState();
 }
@@ -12,17 +13,27 @@ final String title;
 class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
-    Timer(const Duration(seconds: 3), () => {
-    Navigator.pushReplacement(context,MaterialPageRoute(builder: (context) => const MyHomePage(title: "القران الكريم", ))
-    )});
+    Timer(
+        const Duration(seconds: 3),
+        () => {
+              Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const MyHomePage(
+                            title: "القران الكريم",
+                          )))
+            });
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: const Center(
-        child: Image(image: AssetImage("Splash.png"),),
+    return Scaffold(
+      backgroundColor: Color(0xFF195E59),
+      body: const Center(
+        child: Image(
+          image: AssetImage("Splash.png"),
+        ),
       ),
     );
   }
