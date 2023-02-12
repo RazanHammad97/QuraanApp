945d7a3bc1221c464065284d4cfc9095dbbad47e
RazanHammad97 <razanhammadq@gmail.com>
2023-02-13 00:58:21 +0200

    edit the design

--- lib/loadayahs.dart
+++ lib/loadayahs.dart
@@ -2,9 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:quraan_task_razan/QuraanBrain.dart';
 
 class LoadAyahs extends StatefulWidget {
-  const LoadAyahs({Key? key, required String title, required this.surahNum,required this.surahName}) : super(key: key);
-final int surahNum;
-final String surahName;
+  const LoadAyahs(
+      {Key? key,
+      required String title,
+      required this.surahNum,
+      required this.surahName})
+      : super(key: key);
+  final int surahNum;
+  final String surahName;
+
   @override
   State<LoadAyahs> createState() => _LoadAyahsState();
 }
@@ -27,37 +33,58 @@ class _LoadAyahsState extends State<LoadAyahs> {
           if (snapshot.hasData) {
             List items = snapshot!.data as List;
             //print(items.length);
-            return SafeArea(
-              child: SingleChildScrollView(
-                child: Column(
-                  textDirection: TextDirection.rtl,
-                  children: [
-                    Directionality(
-                      textDirection: TextDirection.rtl,
-                      child: AppBar(
-
-                        title: Text("القران الكريم"),
-                      ),
-                    ),
-
-                    SizedBox(
-                      height: 15,
-                    ),
-                    for (var item in items) ...[
+            return Scaffold(
+              body: SafeArea(
+                child: SingleChildScrollView(
+                  child: Column(
+                    textDirection: TextDirection.rtl,
+                    children: [
                       Directionality(
                         textDirection: TextDirection.rtl,
-                        child: Padding(
-                          padding: EdgeInsets.fromLTRB(15, 0, 15, 0),
-                          child: Container(
-                              width: double.infinity,
-
-                            
-                              child:
-                                  RichText(text: TextSpan(text: '${item['text']}'))),
+                        child: AppBar(
+                          title: Text("القران الكريم"),
                         ),
-                      )
-                    ]
-                  ],
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      Text(
+                        '${widget.surahName}',
+                        style:
+                            TextStyle(fontSize: 20, color: Color(0XE2195E59)),
+                      ),
+
+                        Directionality(
+                          textDirection: TextDirection.rtl,
+                          child: Padding(
+                            padding: EdgeInsets.fromLTRB(15, 0, 15, 0),
+                            child: Container(
+                                width: double.infinity,
+                                child: RichText(
+                                    softWrap: true,
+                                    textAlign: TextAlign.center,
+                                    text: TextSpan(
+                                        children:<TextSpan>[
+                                          for (var item in items) ...[
+
+                                          TextSpan(
+                                            style: TextStyle(locale: Locale('ar_AE')),
+                                              text: '${item['numberInSurah']} '),
+                                          TextSpan(
+
+                                              text: ' ${item['text']}'
+                                          ),
+                                          ]]
+                                        )
+
+
+                                )
+                            ),
+                          ),
+                        )
+
+                    ],
+                  ),
                 ),
               ),
             );
